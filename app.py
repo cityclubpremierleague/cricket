172956f9075a0ba3bd7f310e39f237ac3f517cf1
cityclubpremierleague <cityclubpremierleague@gmail.com>
2026-03-18 09:52:21 +0530

    App py change

--- app.py
+++ app.py
@@ -921,7 +921,15 @@ def player_stats(player_id):
     })
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
+
+# Initialize DB tables when the app starts
+with app.app_context():
+    db.create_all()
+
+# Only needed for local development
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
